38653364d0b61dd6b4e2e23965755e9f4f43ac6a
Kyle Lopez <kyalopez@ucsc.edu>
2016-02-13 16:40:34 -0800

    Update Panel adding logic

--- controllers/components.py
+++ controllers/components.py
@@ -2,8 +2,15 @@
 # try something like
 panel_rows = 4
 panel_columns = 4
-height = width = 50
+height = width = 100
 
 
 def Panel():
-    return dict(panel_name = "00", rows = panel_rows, columns = panel_columns, height = height, width = width)
+    return dict(quilt_column=request.vars['col'],
+                 panel_rows=panel_rows,
+                 panel_columns=panel_columns,
+                 height=height,
+                 width=width)
+
+def Quilt():
+    return dict(quilt_row=request.vars['row'])
