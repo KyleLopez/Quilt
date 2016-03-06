4aae7fca32abb2e95e23f043bb51310ef83f58a8
Kyle Lopez <kyalopez@ucsc.edu>
2016-03-06 14:04:39 -0800

    Made it so panels load without full images

--- controllers/components.py
+++ controllers/components.py
@@ -4,11 +4,24 @@ panel_rows = 4
 panel_columns = 4
 height = width = 100
 
+def pad_images(image_rows):
+    n_image_rows = []
+    for row in image_rows:
+        n_image_rows.append(row)
+    n_image_rows.append(None)
+    return n_image_rows
 
 def Panel():
-    images = db().select(db.image.ALL, orderby=db.image.x|db.image.y)
+    start_row = int(request.vars['row']) * panel_rows
+    start_col = int(request.vars['col']) * panel_columns
+    images = db((db.image.x >= start_row) & (db.image.x < start_row + panel_rows) &
+                (db.image.y >= start_col) & (db.image.y < start_col + panel_columns)).select(db.image.ALL, orderby=db.image.x|db.image.y)
+    if (len(images) < panel_rows * panel_columns):
+        images = pad_images(images)
     return dict(panel_rows=panel_rows,
                  panel_columns=panel_columns,
+                 row=start_row,
+                 col=start_col,
                  height=height,
                  width=width,
                  images=images)
